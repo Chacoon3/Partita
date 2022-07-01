5e88e99b85300839074ea983640c8b1e5638a72a
Chacoon3 <dyzily@foxmail.com>
2022-07-01 17:35:47 +0800

    新修改

--- ConsoleProgram/Program.cs
+++ ConsoleProgram/Program.cs
@@ -1,12 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text;
-
-var encoding = Encoding.UTF8;
+var encoding = System.Text.Encoding.UTF8;
 byte[] b = new byte[2];
-for (byte i = 0; i < 254; i++) {
+for (byte i = 0; i < 255; i++) {
     b[0] = i;
-    b[1] = (byte)(i + 1);
-    Console.WriteLine(i + "\t" + "i + 1" + "\t" + encoding.GetString(b));
-
+    Console.WriteLine(i +  "\t" + encoding.GetString(b));
 }
+
