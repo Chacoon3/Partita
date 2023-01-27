f79a23564666267aa80300a7d42bc4647d290212
Chacoon3 <92221220+Chacoon3@users.noreply.github.com>
2023-01-27 17:26:20 -0500

    Delete ConsoleProgram directory

--- ConsoleProgram/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-var encoding = System.Text.Encoding.UTF8;
-byte[] b = new byte[2];
-for (byte i = 0; i < 255; i++) {
-    b[0] = i;
-    Console.WriteLine(i +  "\t" + encoding.GetString(b));    
-}
