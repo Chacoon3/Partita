6fcea8fb840bae38cb711e835e280860ce467cc6
Chacoon3 <92221220+Chacoon3@users.noreply.github.com>
2023-01-27 17:26:55 -0500

    Delete AutomaticMVC directory

--- AutomaticMVC/Interface/IDispatcher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AutomaticMVC.Interface {
-    /// <summary>
-    /// 发布者
-    /// </summary>
-    public interface IPublisher<T> {
-        void Subscribe(ISubscriber<T> subscriber);
-    }
-}
--- AutomaticMVC/Interface/ISubscriber.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AutomaticMVC.Interface {
-    /// <summary>
-    /// 订阅者
-    /// </summary>
-    public interface ISubscriber<T> {
-        void Refresh(T message);
-    }
-}
